3efb2cc420621cc5deef335e843da53d8fc1f6ac
Brian Rosengarten <barosengarten@gmail.com>
2019-01-17 18:29:27 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,9 +1,27 @@
-CREATE TABLE characters (id INTEGER PRIMARY KEY)
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+name TEXT,
+motto TEXT,
+species TEXT,
+author_id INTEGER,
+series_id INTEGER);
 
-CREATE TABLE books (id INTEGER PRIMARY KEY)
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+title TEXT,
+year INTEGER,
+series_id INTEGER);
 
-CREATE TABLE series (id INTEGER PRIMARY KEY)
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INTEGER,
+subgenre_id INTEGER);
 
-CREATE TABLE authors (id INTEGER PRIMARY KEY)
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+name TEXT);
 
-CREATE TABLE sub_genres (id INTEGER PRIMARY KEY)
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+name TEXT);
