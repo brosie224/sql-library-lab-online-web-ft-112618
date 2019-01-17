49abe1a603891870cb91562a6ace26ce5e076c2f
Brian Rosengarten <barosengarten@gmail.com>
2019-01-17 19:32:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,9 +28,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title, COUNT(characters.species) FROM series
 JOIN characters ON series.id = characters.series_id
-WHERE characters.species = "human"
+WHERE characters.species = 'human'
 GROUP BY series.title
-ORDER BY COUNT(characters.species) DESC 
+ORDER BY COUNT(characters.species) DESC
 LIMIT 1;"
 end
 
